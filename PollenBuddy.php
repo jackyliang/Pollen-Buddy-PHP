9b8450ce16005ecf62f1c9f00cdfe8617e0a984e
Jacky Liang <bluelegopiece@hotmail.com>
2015-02-15 22:34:22 +0800

    re #4: added method skeletons

--- PollenBuddy.php
+++ PollenBuddy.php
@@ -2,7 +2,11 @@
 
 class PollenBuddy {
 
-    public $data;
+    private $data;
+    private $city;
+    private $zipcode;
+    private $pollenType;
+    private $fourDayForecast;
 
     const WUNDERGROUND_URL = "http://www.wunderground.com/DisplayPollen.asp?Zipcode=";
 
@@ -13,6 +17,8 @@ class PollenBuddy {
      * @return mixed   $data    Content of the site
      */
     public function PollenBuddy($zipcode) {
+
+        $this->zipcode = $zipcode;
 
         // Initialising cURL
         $ch = curl_init();
@@ -41,10 +47,42 @@ class PollenBuddy {
     }
 
     /**
-     * Get the site HTML
+     * Get the site's HTML data
      * @return mixed The site HTML
      */
     public function getSiteHTML() {
         return $this->data;
+    }
+
+    /**
+     * Get the name of the city
+     * @return String
+     */
+    public function getCity() {
+        return $this->city;
+    }
+
+    /**
+     * Get the zipcode of the city
+     * @return int
+     */
+    public function getZipCode() {
+        return $this->zipcode;
+    }
+
+    /**
+     * Get today's pollen type
+     * @return String
+     */
+    public function getTodayPollenType() {
+        return $this->pollenType;
+    }
+
+    /**
+     * Get the four day forecast data
+     * @return mixed
+     */
+    public function getFourDayForecast() {
+        return $this->fourDayForecast;
     }
 }
